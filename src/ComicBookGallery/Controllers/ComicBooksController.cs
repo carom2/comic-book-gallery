d3e796bd5af3949123313a02f332f8fe0bc5ecc1
carom2 <michael.l.caro@outlook.com>
2017-05-18 11:28:54 -0400

    left off in lesson

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController:Controller
+    {
+        public ContentResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek==DayOfWeek.Monday)
+            {
+
+            }
+            return new ContentResult()
+            {
+                Content = "Hello from the comics book controller!"
+            };
+            
+        }
+    }
+}
